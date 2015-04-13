944c147daa2b130a2f02d2321eabd3e5bd305265
Ricardo Varzim <ricardovarzim@gmail.com>
2015-04-13 19:34:02 +0100

    gitignore

--- src/gameenginecc/GameEngineCC.java
+++ src/gameenginecc/GameEngineCC.java
@@ -1,17 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gameenginecc;
 
 import UDPServer.*;
 
-
-/**
- *
- * @author Ricardo
- */
 public class GameEngineCC {
 
     /**
@@ -19,13 +9,11 @@ public class GameEngineCC {
      */
     public static void main(String[] args) {
         
-        //TODO: MUDAR PARA UDPServer
-        
         UDPServer server = new UDPServer(9000);
         new Thread(server).start();
 
         //System.out.println("Stopping Server");
-        //server.stop();
+        //TODO: server.stop();
     }
     
 }
